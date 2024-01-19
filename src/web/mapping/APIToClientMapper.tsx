2156b184d3dc3ec9f3e024340ab8ad7e27ae2654
Henrik Hofmeister <henrik@newdawn.dk>
2024-01-19 01:37:48 +0100

    fix

--- src/web/mapping/APIToClientMapper.tsx
+++ src/web/mapping/APIToClientMapper.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: MIT
  */
 
-import React, { useMemo } from 'react';
+import React from 'react';
 import type { RESTMethodEdit, RESTResource, RESTResourceSpec } from '../types';
 import type { MappedMethod } from './types';
 import { ItemTypes } from './types';
