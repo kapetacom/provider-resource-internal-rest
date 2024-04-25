092319c82975480fc4e4bc49f5788e57dc8cee65
andertun <harald.andertun@gmail.com>
2024-04-25 15:41:54 +0200

    chore: Remove commented out code

--- src/web/inspectors/requests/StatusCodeFilter.tsx
+++ src/web/inspectors/requests/StatusCodeFilter.tsx
@@ -84,38 +84,3 @@ export const StatusCodeFilter = (props: StatusCodeFilterProps) => {
         </List>
     );
 };
-
-/*
- 
-<List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      {[0, 1, 2, 3].map((value) => {
-        const labelId = `checkbox-list-label-${value}`;
-
-        return (
-          <ListItem
-            key={value}
-            secondaryAction={
-              <IconButton edge="end" aria-label="comments">
-                <CommentIcon />
-              </IconButton>
-            }
-            disablePadding
-          >
-            <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={checked.indexOf(value) !== -1}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ 'aria-labelledby': labelId }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-            </ListItemButton>
-          </ListItem>
-        );
-      })}
-    </List>
-
- */
