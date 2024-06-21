16512f71f80459b7e628eeeaf04033687862c177
andertun <harald.andertun@gmail.com>
2024-06-21 14:19:38 +0200

    fix: Bump deps and make storybook theming work

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,14 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-    stories: ['../stories/**/*.stories.tsx', '../stories/*.stories.tsx', '../stories/*.stories.js'],
+    stories: ['../stories/**/*.stories.tsx', '../stories/*.stories.tsx'],
 
     framework: {
         name: '@storybook/react-webpack5',
         options: {},
     },
 
-    docs: {
-        autodocs: 'tag',
-    },
-
-    addons: ['@storybook/addon-webpack5-compiler-babel', '@chromatic-com/storybook'],
+    addons: ['@storybook/addon-essentials', '@storybook/addon-webpack5-compiler-babel', '@chromatic-com/storybook'],
 };
 
 export default config;
